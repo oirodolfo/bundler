7473df01601497f9cbf66f859edf71fa227a006e
Rod Kisten (Costa) <rodolfocostaclemente@gmail.com>
2026-06-06 18:07:07 -0300

    Update cipo.ts

--- src/cipo.ts
+++ src/cipo.ts
@@ -1,2 +1,9 @@
+/**
+ * @tool Cipo
+ * @global Cipo
+ * @package cipo
+ * @tags css jit atomic userscripts
+ * @description Browser-first atomic CSS runtime and semantic CSS DSL bundled as a standalone browser global.
+ */
 export * from "./cipo/src/index";
 export { default } from "./cipo/src/index";
